2dfe6c599a3f0de50a2161f5195c13bc0f95a332
Andreas Bogk <andreas.bogk@here.com>
2017-03-29 15:39:09 +0200

    Actually, have separate types for binary vs. text streams, and some F# code too.

--- ulib/fs/io.fs
+++ ulib/fs/io.fs
@@ -2,8 +2,10 @@ module FStar.IO
 exception EOF
 open System
 open System.IO
-type fd_read  = TextReader
-type fd_write = TextWriter
+type fd_read      = TextReader
+type fd_write     = TextWriter
+type fd_read_bin  = BinaryReader
+type fd_write_bin = BinaryWriter
 
 let print_newline _ = Printf.printf "\n"
 let print_string x   = Printf.printf "%s" x
@@ -13,6 +15,10 @@ let input_int  ()    = Int32.Parse(System.Console.ReadLine())
 let input_float ()   = Single.Parse(System.Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
 let open_read_file (x:string)  = new StreamReader(x)
 let open_write_file (x:string) = File.CreateText(x)
+let open_read_file_bin (x:string)  =
+  new BinaryReader(File.Open(x, FileMode.Open, FileAccess.Read))
+let open_write_file_bin (x:string) = File.CreateText(x)
+  new BinaryReader(File.Open(x, FileMode.Create, FileAccess.Write))
 let close_read_file (x:fd_read)   = x.Close()
 let close_write_file (x:fd_write) = x.Close()
 let read_line (fd:fd_read)     =
